fc8b9e56b12e12a8ce7aa93448d6ddccc638613b
David Vega <zetacu@gmail.com>
2013-07-20 10:12:56 -0700

    initial configuration

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'ffi-aspell'
+gem 'haml'
+gem 'sass'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+group :test, :development do
+  gem 'pry'
+  gem 'pry-debugger'
+  gem 'rspec-rails'
+  gem 'simplecov'
+  gem 'spork'
+  gem 'spork-rails'
+  gem 'nyan-cat-formatter'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpellCheck::Application.config.secret_token = '1b1f11f5cf1dcc40c3ff17c67b1bbfa481d4fafe1640554b06357d7adf306f7674acfa8711fe15813a6aa4bc2917803603d268a272511ebe3990001833add2c7'
